2c65086468b27c849cddf02e27561e207ee7b763
Lucas Vinicius <lucasviniciuslopez@gmail.com>
2020-10-19 19:12:14 -0300

    Reinventando a roda...

--- bolaoDjango/urls.py
+++ bolaoDjango/urls.py
@@ -1,4 +1,4 @@
-"""bolaoweb URL Configuration
+"""bolaoDjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from sistema.views import landing_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page)
 ]
